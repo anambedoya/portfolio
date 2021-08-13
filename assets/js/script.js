665daf0d2e12750636ee4a3870d07fe4b4ccdc75
Ana Maria Bedoya <anabedoya1997@gmail.com>
2021-08-13 14:22:35 -0500

    Adding scroll sections active link and changing background header

--- assets/js/script.js
+++ assets/js/script.js
@@ -86,10 +86,38 @@ let swiper = new Swiper(".projects__container", {
   });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]');
+console.log(sections);
 
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id');
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector(`.nav__menu a[href*=${sectionId}]`).classList.add('active-link');
+        } else {
+            document.querySelector(`.nav__menu a[href*=${sectionId}]`).classList.remove('active-link');
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive);
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
+function scrollHeader() {
+    const nav = document.getElementById('header');
+    // When the scroll is greater than 80 viewport height, add the scroll-header class
+    if(this.scrollY >= 80)
+        nav.classList.add('scroll-header');
+    else
+        nav.classList.remove('scroll-header');
+}
 
+window.addEventListener('scroll', scrollHeader);
 
 /*==================== SHOW SCROLL UP ====================*/ 
 
